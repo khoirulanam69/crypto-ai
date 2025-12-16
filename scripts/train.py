77386c31a81ffcfcef25dcf46fd14882a170c4eb
khoirulanam69 <khoirulanam.um@gmail.com>
2025-12-16 20:01:29 +0700

    fix : error train

--- scripts/train.py
+++ scripts/train.py
@@ -1,4 +1,7 @@
+import sys
 import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 import pandas as pd
 from stable_baselines3 import PPO
 from stable_baselines3.common.vec_env import DummyVecEnv
